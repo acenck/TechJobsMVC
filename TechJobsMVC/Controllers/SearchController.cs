d4ecd5f2cf5b76df2f076ff5f18577a546183047
acenck <nickacinelli@gmail.com>
2021-01-03 02:47:25 -0600

    altered search/list controllers, removed unnecessary html to properly format list table.

--- TechJobsMVC/Controllers/SearchController.cs
+++ TechJobsMVC/Controllers/SearchController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using TechJobsMVC.Data;
+using TechJobsMVC.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -17,6 +19,25 @@ namespace TechJobsMVC.Controllers
             return View();
         }
 
-        // TODO #3: Create an action method to process a search request and render the updated search view. 
+        // TODO #3: Create an action method to process a search request and render the updated search view. Complete.
+
+        public IActionResult Results(string searchType, string searchTerm)
+        {
+            List<Job> jobs;
+            if (String.IsNullOrEmpty(searchTerm))
+            {
+                jobs = JobData.FindAll();
+                ViewBag.Title = "All jobs";
+            }
+            else
+            {
+                jobs = JobData.FindByColumnAndValue(searchType, searchTerm);
+                ViewBag.title = "Jobs with " + ListController.ColumnChoices[searchType] + ": " + searchTerm;
+            }
+            ViewBag.jobs = jobs;
+            ViewBag.columns = ListController.ColumnChoices;
+
+            return View("Index");
+        }
     }
 }
